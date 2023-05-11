3bf4ebdc37a2a9c448efcd158721731851847d14
JoelMRenaud <joelmrenaud@gmail.com>
2023-05-11 08:53:58 -0700

    1

---
+++ app.py
@@ -0,0 +1,22 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+
+# Custom filter
+app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure app to use SQLite database
+db = SQL("sqlite:///database.db")
+
+@app.route("/")
+def home():
+    return render_template("home.html")
