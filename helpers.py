801039a5d1dbdf254092eb523f31031a0d5259de
JoelMRenaud <joelmrenaud@gmail.com>
2023-05-19 15:16:19 -0700

    top fixed

--- helpers.py
+++ helpers.py
@@ -15,10 +15,4 @@ def login_required(f):
 
 
 def apology(message, code=400):
-    """Render message as an apology to user."""
-    def escape(s):
-        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
-                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
-            s = s.replace(old, new)
-        return s
-    return render_template("apology.html", top=code, bottom=escape(message)), code
+    return render_template("apology.html", message=message)
