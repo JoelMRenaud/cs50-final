d497733e2a236f6dbbc30a143e4e18c479bc413b
JoelMRenaud <joelmrenaud@gmail.com>
2023-06-25 16:34:04 -0700

    like 13.5

--- app.py
+++ app.py
@@ -30,9 +30,7 @@ def home():
         if like:
             db.execute("UPDATE images SET likes = likes + 1 WHERE id = ?", like)
             alreadyliked = db.execute("SELECT * FROM like WHERE image_id = ? AND user_id = ?", like, session["user_id"])
-            if not alreadyliked:
-                db.execute("UPDATE images SET likes = likes + 1 WHERE id = ?", like)
-                db.execute("INSERT INTO like (image_id, user_id) VALUES (?, ?)", like, session["user_id"])
+            db.execute("INSERT INTO like (image_id, user_id) WHERE image_id = ? AND user_id = ?", like, session["user_id"])
     if session["user_id"] == 1:
         admin = 1
     else:
