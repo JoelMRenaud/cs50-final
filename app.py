8c0230551b82494e04f9de54143e70b53c61a30f
JoelMRenaud <joelmrenaud@gmail.com>
2023-06-26 10:52:18 -0700

    total 2.1

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def home():
     """Check for total amount likes for cats and dogs"""
     dog = db.execute("SELECT SUM(likes) FROM images WHERE animal = ?", "dog")
     cat = db.execute("SELECT SUM(likes) FROM images WHERE animal = ?", "cat")
-    return render_template("home.html", images=rows, admin=admin)
+    return render_template("home.html", images=rows, admin=admin, cat=cat, dog=dog)
     
 
 @app.route("/post", methods=["GET", "POST"])
